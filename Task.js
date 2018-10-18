20a353679461475ff7014e1a2ed9056700e85998
Quinn Roberts <quinn@jelly-snakes.com>
2018-10-18 11:41:59 +1100

    Convert fields from plain fields on a class, into classes themselves. This moves the implementation from the main task class into individual classes and means we can simplify all the switch-cases

--- Task.js
+++ Task.js
@@ -2,33 +2,76 @@ const {categories} = require('./Globals');
 const requester = require('./ApiRequester');
 const Promise = require('bluebird');
 
+class TaskField {
+    constructor(googleHandler, initialValue) {
+        this.parseGoogle = this._loadArgument(googleHandler);
+        this.value = initialValue || null;
+    }
+
+    _loadArgument(argument) {
+        if (typeof argument === "string") {
+            return (data) => data[argument];
+        } else if (typeof argument === "function") {
+            return argument;
+        } else {
+            throw new TypeError(`Field (${argument}) is not a String or Function`);
+        }
+    }
+
+    loadFromGoogle(data) {
+        this.value = this.parseGoogle(data, this.value)
+    }
+
+    loadFromTrello() {
+
+    }
+
+    getValue() {
+        return this.value
+    }
+
+    setValue(data) {
+        this.value = data;
+    }
+}
+
+class BasicTaskField extends TaskField {
+    constructor(googleHandler, trelloHandler, initialValue) {
+        super(googleHandler, initialValue);
+        this.parseTrello = this._loadArgument(trelloHandler);
+        this.value = null;
+    }
+
+    loadFromTrello(data) {
+        this.value = this.parseTrello(data)
+    }
+}
+
+class CustomTaskField extends TaskField {
+    constructor(googleHandler, fieldIds, trelloHandler, initialValue) {
+        super(googleHandler, initialValue);
+        if (typeof fieldIds === 'string') {
+            fieldIds = [fieldIds];
+        }
+        this.fieldIds = fieldIds;
+        this.parseTrello = this._loadArgument(trelloHandler);
+        this.value = null;
+    }
+
+    doesFieldMatch(field) {
+        return field.idCustomField in this.fieldIds
+    }
+
+    loadFromTrello(field) {
+        this.value = this.parseTrello(field.value, value)
+    }
+}
+
 /**
  * Represents a single task.
  */
 class Task {
     constructor() {
-        /* Ids */
-        this.googleId = null;
-        this.trelloId = null;
-
-        /* Core data */
-        this.name = "";
-        this.description = "";
-        this.status = 1;
-        this.maxInstances = 1;
-        this.mentors = [];
-        this.tags = [];
-        this.isBeginner = false;
-        this.categories = {};
-        this.days = 3;
-        this.externalUrl = "";
-
-        /* Extraneous data */
-        this.lastModified = "";
-        this.claimedCount = 0;
-        this.availableCount = 0;
-        this.completedCount = 0;
-
         /* Trello Constants */
         this.customFields = {
             isBeginner: "5bb13d35adbb5244eb5b749d",
@@ -42,6 +85,38 @@ class Task {
             isDocs: "5bb13da34edf5926c83f294a",
             isQa: "5bb13da9f285397f1a80dddc",
             isOutResearch: "5bb13dbc351a9c4e5c93cd1f"
+        };
+
+        /**
+         * All the fields for a task
+         * @type {{TaskField}}
+         */
+        this.fields = {
+            googleId: new CustomTaskField('id', this.customFields.googleId, field => parseInt(field.number)),
+            trelloId: new BasicTaskField((data, value) => value || data['private_metadata'], 'id'),
+
+            name: new BasicTaskField('name', 'name'),
+            description: new BasicTaskField('description', 'desc'),
+            status: new TaskField('status'),//TODO: Add this to trello cards
+            maxInstances: new CustomTaskField('max_instances', this.customFields.instances, field => parseInt(field.number)),
+            mentors: new TaskField('mentors'),//TODO: Add this to trello cards
+            tags: new CustomTaskField('tags', this.customFields.tags, field => field.text.split(/\s*,\s*/i)),
+            isBeginner: new CustomTaskField('is_beginner', this.customFields.isBeginner, field => field.checked === "true"),
+            externalUrl: new TaskField('external_url'), //TODO: Add this to trello cards
+            days: new CustomTaskField('time_to_complete_in_days', this.customFields.days, field => parseInt(field.number)),
+
+            categories: new TaskField((data, prior) => data.categories.forEach(item => prior[item] = true), {}),
+
+            lastModified: new TaskField('last_modified'),
+            claimedCount: new TaskField('claimed_count'),
+            availableCount: new TaskField('available_count'),
+            completedCount: new TaskField('completed_count')
+
+        };
+
+        for (let field in this.fields) {
+            this.__defineGetter__(field, this.fields[field].getValue);
+            this.__defineSetter__(field, this.fields[field].setValue);
         }
     }
 
@@ -51,27 +126,9 @@ class Task {
      * @param data {json} The google api result for this task.
      */
     loadFromGoogle(data) {
-        this.name = data.name;
-        this.googleId = data.id;
-        this.description = data.description;
-        this.status = data.status;
-        this.maxInstances = data.max_instances;
-        this.mentors = data.mentors;
-        this.tags = data.tags;
-        this.isBeginner = data.is_beginner;
-        this.days = data.time_to_complete_in_days;
-        this.externalUrl = data.external_url;
-
-        data.categories.forEach((item) => this.categories[item] = true);
-
-        /* Extraneous data */
-        this.lastModified = data.last_modified;
-        this.claimedCount = data.claimed_count;
-        this.availableCount = data.available_count;
-        this.completedCount = data.completed_count;
-
-        /* Update trello id if we have one. */
-        this.trelloId = data.private_metadata || this.trelloId;
+        for (let field in this.fields) {
+            this.fields[field].loadFromGoogle(data)
+        }
     }
 
     /**
@@ -90,9 +147,11 @@ class Task {
          *
          * Returns a promise that only activates when all fields have been requested
          */
-        this.trelloId = data.id;
-        this.description = data.desc;
-        this.name = data.name;
+
+        /* Load all the basic fields */
+        Object.values(this.fields)
+            .filter(value => BasicTaskField.isPrototypeOf(value))
+            .forEach(field => field.loadFromTrello(data));
 
         //TODO: Get the custom field data at the same time as the card data
         return requester.getCustomFields(this.trelloId).then(body => {
@@ -132,6 +191,13 @@ class Task {
      * @param field {json} The custom field to load the data from
      */
     handleField(field) {
+        Object.values(this.fields)
+            /* Only process fields linked with a Custom Field */
+            .filter(taskField => CustomTaskField.isPrototypeOf(taskField))
+            /* Only process field that match this field's id */
+            .filter(taskField => taskField.doesFieldMatch(field))
+            /* Load the data for each remaining field */
+            .forEach(taskField => taskField.loadFromTrello(field));
         delete this.unloadedFields[field.idCustomField];
         //TODO: This is really ugly
         switch (field.idCustomField) {
