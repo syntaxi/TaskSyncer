6990a77bbc082cfad3ee04b9533798407ac0909b
Quinn Roberts <quinn@jelly-snakes.com>
2019-11-06 17:17:07 +0800

    Add method to tasklist to delete a task and find a task matching a predicate

--- TaskList.js
+++ TaskList.js
@@ -3,8 +3,12 @@ const Task = require('./Task');
 const trelloInterface = require("./TrelloInterface.js");
 const googleInterface = require("./GoogleInterface.js");
 
+/**
+ * @callback TaskMatcher
+ * @param task {Task} The task to match
+ * @return {boolean} True if the task matched, false otherwise
+ */
 class TaskList {
-
 
     constructor() {
         /**
@@ -37,6 +41,41 @@ class TaskList {
             index = this.tasks.length - 1;
         }
         return this.tasks[index];
+    }
+
+    /**
+     * Gets a task matching the given predicate
+     *
+     * @param matching {TaskMatcher} The predicate to check each card against
+     * @return {Task|undefined} A matching car dif there is one, undefined if there is not
+     */
+    getTask(matching) {
+        return this.tasks.find(matching);
+    }
+
+    /**
+     * Deletes the first task matching the predicate
+     * @param matching {TaskMatcher} The predicate to check each task against
+     * @return {boolean} True if the task was deleted, false otherwise.
+     */
+    deleteTask(matching) {
+        let matchingIndex = this.tasks.findIndex(matching);
+        if (matchingIndex >= 0) {
+            this.tasks.splice(matchingIndex, 1);
+            return true;
+        } else {
+            // TODO error? Need to decide on a universal method
+            return false;
+        }
+    }
+
+    /**
+     * Deletes a given task from the list
+     * @param task The task to delete
+     * @return {boolean} True if it was deleted false otherwise
+     */
+    deleteThisTask(task) {
+        return this.deleteTask(other => other === task);
     }
 
     /**
@@ -73,6 +112,7 @@ class TaskList {
         return apiInterface.writeAllTasks(this)
             .tap(() => propagateIds ? this._propagateIds() : undefined)
     }
+
 }
 
 module.exports = new TaskList();
