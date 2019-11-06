f508b6f78790c5e8afffd4c8e61d3419199671fb
Quinn Roberts <quinn@jelly-snakes.com>
2019-11-06 17:53:40 +0800

    Add in updating webhook(s) as needd

--- TrelloMonitor.js
+++ TrelloMonitor.js
@@ -2,12 +2,14 @@ const {categoryLists, callbackUrl, boardId, botMemberId} = require("./config.jso
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const requester = require("./TrelloApiRequester.js");
 class TrelloMonitor {
     /**
      * @inheritDoc
      */
     setupMonitoring(taskList) {
         this._createExpressApp();
+        requester.getTrelloWebhooks().then(this._refreshWebhooks);
         this.monitoredList = taskList;
     }
 
@@ -17,7 +19,45 @@ class TrelloMonitor {
      * @param body {WebhookAction}
      */
     onWebhookActivate(body) {
+    /**
+     * Updates all trello webhooks.
+     *
+     * This will update any existing ones to point to the correct callback,
+     * delete any no longer valid/relevant ones,
+     * and create a new webhook for each element lacking one
+     *
+     * At present only the board requires a webhook
+     *
+     * @param webhooks The webhooks to refresh
+     * @return {Promise<void>}
+     * @private
+     */
+    async _refreshWebhooks(webhooks) {
+        console.log("Processing trello webhooks");
+        let elements = new Set();
+        elements.add(boardId);
+        for (let webhook of webhooks) {
+            if (elements.has(webhook.idModel)) {
+                if (webhook.callbackURL !== callbackUrl) {
+                    await requester.updateTrelloWebhook(webhook.id, webhook.idModel);
+                    console.log(`Webhook for '${webhook.idModel}' updated`);
+                } else {
+                    console.log(`Webhook for '${webhook.idModel}' unchanged`);
+                }
+                elements.delete(webhook.idModel);
+            } else {
+                await requester.deleteTrelloWebhook(webhook.id);
+                console.log(`Webhook for '${webhook.idModel}' deleted`);
+            }
+        }
+        for (let category of elements) {
+            await requester.createTrelloWebhook(category);
+            console.log(`Webhook for '${category}' created`);
+        }
     }
+
+
+
     /**
      * Starts an express server to listen for webhooks on port 3000
      *
