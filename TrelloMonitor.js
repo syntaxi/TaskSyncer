e479c1d50d38e78f526411a934a152f59b4543dc
Quinn Roberts <quinn@jelly-snakes.com>
2019-11-06 17:56:01 +0800

    Add in logic handling for each of the cases in the prior commit (Cards body updated, fields updated, moved, created & deleted)

--- TrelloMonitor.js
+++ TrelloMonitor.js
@@ -44,6 +44,132 @@ const requester = require("./TrelloApiRequester.js");
  *
  */
 class TrelloMonitor {
+
+    /**
+     * Handles a new card being added to the published lists on trello.
+     *
+     * @param card {IdNameTuple} The card that was created.
+     */
+    onCardCreated(card) {
+        //TODO propagate this change to google
+        requester.getCard(card.id).then(rawCard => {
+            let task = this.monitoredList.getOrMakeTask(task => trelloInterface.doesTaskMatchData(task, rawCard));
+            task.listCategoryAdded = false;
+            trelloInterface.loadIntoTask(rawCard, task);
+            if (task.listCategoryAdded) {
+                return trelloInterface.propagateCategoryChange(task);
+            }
+            console.log(`Card '${card.name}' (${card.id}) created locally`);
+        });
+    }
+
+    onCardDeleted(card) {
+        //TODO propagate this change to google
+        //TODO implement
+        console.log(`Card '${card.name}' (${card.id}) deleted locally`);
+    }
+
+    /**
+     *
+     * @param card {{name: string, desc: string, id:string}}
+     */
+    onMainChanged(card) {
+        let task = this.monitoredList.getTask(task => task.getField(fields.TRELLO_ID) === card.id);
+        if (task) {
+            //TODO propagate this change to google
+            task.setField(fields.NAME, card.name);
+            task.setField(fields.DESCRIPTION, card.desc);
+            console.log(`Card '${card.name}' (${card.id}) update handled.`)
+        } else {
+            console.error(`Could not match card in webhook '${card.id}' (${card.name}) with any known task`);
+        }
+    }
+
+    /**
+     * Note value == NULL when unchecked
+     * @param customFieldId {IdNameTuple}
+     * @param customFieldVal {RawCustomField}
+     * @param card {IdNameTuple}
+     */
+    onCustomChanged(customFieldId, customFieldVal, card) {
+        let task = this.monitoredList.getTask(task => task.getField(fields.TRELLO_ID) === card.id);
+        if (task) {
+            //TODO propagate this change to google
+            let category = -1;
+            let fieldVal = trelloInterface.customFieldToValue(customFieldVal);
+            switch (customFieldId.id) {
+                //TODO: make custom field handling globally "better"
+                case "5da6fbfff25f736fcdec8cfb":
+                    task.setField(fields.IS_BEGINNER, customFieldVal);
+                    break;
+                case "5da6fbfff25f736fcdec8cef":
+                    task.setField(fields.DAYS, fieldVal);
+                    break;
+                case "5da6fbfff25f736fcdec8ced":
+                    task.setField(fields.TAGS, fieldVal);
+                    break;
+                case "5da6fbfff25f736fcdec8ceb":
+                    task.setField(fields.MAX_INSTANCES, fieldVal);
+                    break;
+                case "5da6fbfff25f736fcdec8ce8":
+                    task.setField(fields.GOOGLE_ID, fieldVal);
+                    break;
+                case "5da6fbfff25f736fcdec8cf9":
+                    category = categories.CODING;
+                    break;
+                case "5da6fbfff25f736fcdec8cf7":
+                    category = categories.DESIGN;
+                    break;
+                case "5da6fbfff25f736fcdec8cf5":
+                    category = categories.DOCS_TRAINING;
+                    break;
+                case "5da6fbfff25f736fcdec8cf3":
+                    category = categories.QA;
+                    break;
+                case "5da6fbfff25f736fcdec8cf1":
+                    category = categories.OUTRESEARCH;
+                    break;
+                default:
+                    console.error(`Unknown custom field '${customFieldId.name}' (${customFieldId.id}) in webhook`)
+            }
+
+            if (category >= 0) {
+                if (fieldVal) {
+                    // Gaining a new category
+                    task.addCategory(category);
+                } else {
+                    //TODO Stop removing the category for the list the card is in
+                    task.removeCategory(category);
+                }
+            }
+            console.log(`Card '${card.name}' (${card.id}) custom field handled.`)
+        } else {
+            console.error(`Could not match card '${card.id}' (${card.name}) in webhook with any known task`);
+        }
+    }
+
+    /**
+     *
+     * @param oldList {IdNameTuple} The list the card was moved from
+     * @param newList {IdNameTuple} The list the card was moved to
+     * @param card {IdNameTuple} The card that was moved
+     */
+    onCardMoved(oldList, newList, card) {
+        let task = this.monitoredList.getTask(task => task.getField(fields.TRELLO_ID) === card.id);
+        if (task) {
+            //TODO propagate this change to google
+            task.removeCategory(parseInt(catLookup[oldList.id]));
+            task.addCategory(parseInt(catLookup[newList.id]));
+
+            // Replicate this category change
+            trelloInterface.propagateCategoryChange(task);
+
+            console.log(`Card '${card.name}' (${card.id}) moved from list '${oldList.name}' to list '${newList.name}'`)
+        } else {
+            console.error(`Could not match card '${card.id}' (${card.name}) in webhook with any known task`);
+        }
+    }
+
     /**
      * @inheritDoc
      */
